4d282f2598b551865daae34218362e10c6076cbe
BRUNORAJA <35687885+BRUNORAJA@users.noreply.github.com>
2018-01-31 14:22:43 +0530

    c program
    negative or positive

---
+++ adding of two numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n the positive or negative number");
+scanf("\n%d%d",&a,&b)_;
+if(a>b)
+{
+
+printf("\nThe number is positive");
+}
+else
+{
+printf("\n the number is negative");
+}
+getch();
+}
