210dde4237943a16b01b7021444b2c9a6f3ea600
BRUNORAJA <35687885+BRUNORAJA@users.noreply.github.com>
2018-01-31 14:56:32 +0530

    bru.c
    character

---
+++ constant.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[4];
+int a,b;
+clrscr();
+printf("\n the constant program");
+scanf("\n%d%d",&a,&b);
+if(a>=b)
+{
+printf("\nthe number is constant");
+}
+else
+{
+printf("\n the number is not constant");
+}
+getch();
+}
