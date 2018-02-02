b4692bfcdf696b92d88d88a6a8af30b0e8e99e46
BRUNORAJA <35687885+BRUNORAJA@users.noreply.github.com>
2018-02-02 16:11:05 +0530

    find biggest number

---
+++ bigest number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+scanf("%d%d",&a&b);
+if(a>b)
+{
+printf("\n the A value is higher than the B value%d",a);
+}
+else
+{
+printf("\n the value is higher than the a%d",b);
+}
+getch();
+}
