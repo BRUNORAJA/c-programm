599793207faf54005728e9e780adbaf56ec1ca1e
BRUNORAJA <35687885+BRUNORAJA@users.noreply.github.com>
2018-02-02 16:56:02 +0530

    Update 12.c

--- 12.c
+++ 12.c
@@ -8,7 +8,7 @@ void main()
 
     originalInteger = n;
 
-    // reversed integer is stored in variable 
+    
     while( n!=0 )
     {
         remainder = n%10;
@@ -16,7 +16,7 @@ void main()
         n /= 10;
     }
 
-    // palindrome if orignalInteger and reversedInteger are equal
+    
     if (originalInteger == reversedInteger)
         printf("%d is a palindrome.", originalInteger);
     else
