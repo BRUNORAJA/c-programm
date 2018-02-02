61373f818e91dd65e0403e6fd00eaff5300bc4f3
BRUNORAJA <35687885+BRUNORAJA@users.noreply.github.com>
2018-02-02 16:20:49 +0530

    leap year

---
+++ leap year.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+a=100,b=4;
+if(a/b==0)
+{
+printf("\n the year is leap");
+}
+else
+{
+printf("\n the year is not leap");
+}
+getch();
+}
